c0758a4b626204653a4bb510fdfdede88c50b646
mlntcandy <me@mlntcandy.com>
2024-05-10 18:14:44 +0300

    handle request errors

--- common.php
+++ common.php
@@ -32,16 +32,20 @@ function ntidRequest($conf, $type, $body)
     $ntid_url = rtrim($ntid_url, '/');
     $ntid_url = $ntid_url . '/api/external/auth';
 
-    $response = ntidSendWebPostRequest($ntid_url, json_encode([
-        'action' => $type,
-        'client_id' => $conf['ntid_client_id'],
-        'client_secret' => $conf['ntid_client_secret'],
-        ...$body
-    ]));
+    try {
+        $response = ntidSendWebPostRequest($ntid_url, json_encode([
+            'action' => $type,
+            'client_id' => $conf['ntid_client_id'],
+            'client_secret' => $conf['ntid_client_secret'],
+            ...$body
+        ]));
 
-    if ($response === null) {
+        if ($response === null) {
+            return null;
+        }
+
+        return json_decode($response, true);
+    } catch (Exception $e) {
         return null;
     }
-
-    return json_decode($response, true);
 }
